a78e973365c03e127f1c53b11b620a308e1e9066
Abhishek Kumar <abhi_jimmy@hotmail.com>
2026-06-16 17:56:38 +0530

    Exception logger and utils added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "visa",
+    version = "0.0.0",
+    author = "Abhishek Kumar",
+    author_email = "abhi_jimmy@hotmail.com",
+    packages=find_packages()
+)
